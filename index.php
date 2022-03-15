7c0a11f329afd9e8e84e775cf1cc4e43b5dd6daa
TIM BIN <tim.bin@pnc.lan>
2022-03-15 20:26:39 +0700

    #4 edit style

--- index.php
+++ index.php
@@ -10,17 +10,17 @@ require_once ("templates/header.php");
     <div class="container-fluid">
         <!-- LOGO FB -->
         <div class="navbar-header">
-            <a class="navbar-brand" href="#"><img src="images/icon-facebook.webp" alt="" width="30%"></a>
+            <a class="navbar-brand" href="#"><img src="images/icon-facebook.webp" alt="" width="25%"></a>
         </div>
         <!-- page home and group -->
         <div class="nav_pages">
-            <li><a href="#"><i class="fa fa-home" style="font-size:30px"></i></a></li>
-            <li><a href="#"><i class="fa fa-group" style="font-size:30px"></i></a></li>
+            <li class="active"><a href="#"><i class="fa fa-home" style="font-size:25px"></i></a></li>
+            <li><a href="#"><i class="fa fa-group" style="font-size:22px"></i></a></li>
         </div>      
         <!-- ICON USER NAME -->
         <div class="nav_icons">
-            <li class="cicle-user"><a href="#" ><i class="fa fa-user" style="font-size:30px"></i></a></li>
-            <li class="cicle-user"><a href="#" ><i class="fa fa-caret-down" style="font-size:30px"></i></a></li>
+            <li class="d-flex"> <span class="cicle-user"><a href="#" ><i class="fa fa-user" style="font-size:20px"></i></a></span> <span>username</span></li>
+            <li class="cicle-user"><a href="#" ><i class="fa fa-caret-down" style="font-size:20px"></i></a></li>
         </div>
     </div>
 </nav>
