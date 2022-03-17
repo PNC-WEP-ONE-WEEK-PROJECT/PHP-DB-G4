7daea56f3a440154c8754d5a150448e10dd57a3f
SAUTH.PHOUEK <SAUTH.PHOUEK@PNC.LAN>
2022-03-17 16:09:36 +0700

    update post

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
 // SHOW BOX TO EDIT OR DELETE POST---------------------------
-let get_activities = document.querySelector(".card-activity");
-get_activities.style.display = "none";
-function showActivity(){
-    console.log(get_activities);
-    return show(get_activities);
-}
 
 
 function cancel_post(){
@@ -12,6 +6,7 @@ function cancel_post(){
     document.body.style.overflow="visible";
     return hide(cancel_post);
 }
+
 
 function show(element){
     element.style.display = "block";
@@ -21,27 +16,27 @@ function hide(element){
     element.style.display = "none";
 }
 
-window.onclick = function(event) {
-    if (event.target.className !== "icon") {
-        get_activities.style.display = "none";
-    }
-}
-
-let more_icon = document.querySelector(".fa-ellipsis-h");
-more_icon.addEventListener("click",showActivity);
 // CREATE POST---------------------------------------------------------------
 let write_post = document.querySelector(".add-post");
 function create_post(){
     if (add_contianer_post.style.display== "none"){
         document.body.style.overflow="hidden";
         show(add_contianer_post);
-       
     }
 }
 
 let add_contianer_post=document.querySelector(".container");
 
-let posts=document.querySelectorAll(".container-card")
-posts.forEach(element => {
-    console.log(element);
-});
+
+window.onclick = function (event) {
+    if(event.target.matches(".icon")){
+        event.target.parentElement.nextElementSibling.style.display = "block";
+    } else{
+        let card_mores = document.querySelectorAll(".card-activity");
+        for(let card of card_mores){
+            card.style.display = "none";
+        }
+    }
+}
+
+window.reload();
