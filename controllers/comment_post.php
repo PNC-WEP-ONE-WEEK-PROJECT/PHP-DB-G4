15ca1ca98e86976cb63c2fdf8a37058e91b912d4
SAUTH.PHOUEK <SAUTH.PHOUEK@pnc.lan>
2022-03-21 23:02:17 +0700

    edit comment

--- controllers/comment_post.php
+++ controllers/comment_post.php
@@ -3,9 +3,10 @@ require_once("../models/database.php");
 if (!empty($_POST["comment"])){
 
     $get_Comment = $_POST['comment'];
+    $user_id = $_POST["user"];
     $get_post_id = $_GET["id"];
-    $user_id = 
-    comment_post($get_Comment,$get_post_id ,1);
+
+    comment_post($get_Comment,$get_post_id ,$user_id);
 }
 header('location: ../index.php');
 ?>
--- controllers/delete_comm.php
+++ controllers/delete_comm.php
@@ -5,7 +5,7 @@
 require_once('../models/database.php');
 $id= $_GET['id']; 
 delete_comment($id);
-echo $id;
 
 
-header("location: /index.php?id=$id");
+
+header("location: /index.php");
